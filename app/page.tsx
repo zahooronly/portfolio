90167ce96150f54638a217738ab359d31485bbf1
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-08 17:48:49 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-between">
+      <h1 className="text-8xl font-bold font-mono">WILL BE PUBLIC SOON</h1>
+    </main>
+  );
+}
