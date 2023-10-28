7ddcaea3709feee5bcfd4e089bd59c3f215f11b4
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-10-28 10:32:57 +0500

    portfolio

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex h-screen items-center justify-between">
-      <h1 className="text-8xl font-bold font-mono">WILL BE PUBLIC SOON</h1>
-    </main>
-  );
-}
