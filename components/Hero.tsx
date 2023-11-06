7b4c6a159eb894323337e8ff7813caed7c3fe7b9
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-11-06 17:47:41 +0500

    logo set

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,16 +9,18 @@ const Hero = () => {
           className="border rounded-full shadow-md"
           src="/images/gamistan.png"
           alt="Zahoor Ahmad"
-          width={100}
-          height={100}
+          width={200}
+          height={200}
         />
-        <h1 className="font-light text-xl text-center">
+        <h1 className="font-light text-xl text-center text-slate-800">
           Mr<span className="font-bold">.</span> Zahoor Ahmad
           <span className="font-bold text-xl">.</span>
         </h1>
       </div>
-      <h1 className="text-4xl font-semibold">Full-Stack Web Developer</h1>
-      <p className="text-sm ">
+      <h1 className="text-4xl font-semibold text-slate-500">
+        Full-Stack Web Developer
+      </h1>
+      <p className="text-sm text-slate-500">
         Imma passionate Developer. I enjoy making Websites that help people.
       </p>
     </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import GetMyCV from "./Button";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between border-b-2 pb-2">
-      <h1>LOGO</h1>
+      <Image
+        src="/images/zahoor.png"
+        alt="Zahoor Ahmad"
+        width={50}
+        height={50}
+      />
       <GetMyCV>Get My CV</GetMyCV>
     </div>
   );
