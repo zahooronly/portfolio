c8e6e71af205f506879f943b06edbff9b5198535
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-11-10 18:43:36 +0500

    Logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between border-b-2 pb-2">
       <Image
+        className="shadow-md rounded-full"
         src="/images/zahoor.png"
         alt="Zahoor Ahmad"
         width={50}
