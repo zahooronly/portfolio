31c9710c76057d88c87a78560bf35a0f06c84680
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-11-13 12:17:38 +0500

    Endline Component Made

---
+++ components/EndLine.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EndLine = () => {
+  return (
+    <div>
+      <hr className="my-8 sm:my-12 border-2 rounded-xl w-2/4 mx-auto" />
+    </div>
+  );
+};
+
+export default EndLine;
