86cf0a1e04c1001744bb9b8f1e470ee6604dd3bf
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-11-26 12:51:43 +0500

    Project-1

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,54 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProjectCardProps {
+  projectName: string;
+  projectDescription: string;
+  projectLink: string;
+  projectImage: string;
+}
+
+const ProjectCard = ({
+  projectName,
+  projectDescription,
+  projectLink,
+  projectImage,
+}: ProjectCardProps) => {
+  return (
+    <HoverCard>
+      <HoverCardTrigger>
+        <Card>
+          <CardHeader>{projectName}</CardHeader>
+          <CardDescription>{projectDescription}</CardDescription>
+          <CardContent>
+            <Link href={projectLink} target="_blank">
+              <Image
+                src={projectImage}
+                alt={projectName}
+                width={500}
+                height={300}
+              />
+            </Link>
+          </CardContent>
+        </Card>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-[90vw] h-[90vh] flex justify-center items-center">
+        <iframe src={projectLink} className="w-[100%] h-[100%]"></iframe>
+      </HoverCardContent>
+    </HoverCard>
+  );
+};
+
+export default ProjectCard;
