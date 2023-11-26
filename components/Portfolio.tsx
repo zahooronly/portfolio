97e4edd703cdac7d89f35a719030821471e0adec
ZahoorOnly <i.am.zahoor.a@outlook.com>
2023-11-26 12:51:50 +0500

    Portfolio

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,9 +1,19 @@
+import ProjectCard from "./ProjectCard";
+
 const Portfolio = () => {
   return (
     <div>
       <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-slate-500 dark:text-slate-100">
         Projects.
       </h2>
+      <div>
+        <ProjectCard
+          projectImage="/images/Project-1.png"
+          projectName="Portfolio Website"
+          projectDescription="I have used Next.js, Tailwind CSS, TypeScript, and Vercel for deployment."
+          projectLink="https://zahooronly.vercel.app"
+        />
+      </div>
     </div>
   );
 };
